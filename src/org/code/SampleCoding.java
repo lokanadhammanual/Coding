7b7b073add61ce2fa9e8c8c6b2df7075da22f0a7
lokanadhammanual <lokanadhammanual@gmail.com>
2023-01-11 09:23:38 +0530

    modified

--- src/org/code/SampleCoding.java
+++ src/org/code/SampleCoding.java
@@ -4,5 +4,6 @@ public class SampleCoding {
 public static void main(String[] args) {
 	System.out.println("Java Coding Starts......");
 	System.out.println("Selenium Coding Starts");
+	System.out.println("FrameWork Session Starts.....");
 }
 }
