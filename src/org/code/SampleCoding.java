cf4c5830296d86a3138da9eeec8cb2c3cbdc4bd8
lokanadhammanual <lokanadhammanual@gmail.com>
2023-01-11 09:02:13 +0530

    modify

--- src/org/code/SampleCoding.java
+++ src/org/code/SampleCoding.java
@@ -3,5 +3,6 @@ package org.code;
 public class SampleCoding {
 public static void main(String[] args) {
 	System.out.println("Java Coding Starts......");
+	System.out.println("Selenium Coding Starts");
 }
 }
